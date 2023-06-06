478bf750df7b8bfd5d73414e6280aef7ec48de7c
Veronika <16nika94@mail.ru>
2023-06-06 23:36:12 +0300

    Homework task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите число М");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] NewArray ()
+{
+int[] array =new int [size];
+for (int i=0; i <size; i++)
+{
+    Console.WriteLine("Введите число");
+   array [i] = Convert.ToInt32(Console.ReadLine());
+    
+    }
+    return array;
+    }
+   int []array = NewArray ();
+   int PositiveNumbers =0;
+   for (int i=0; i <size; i++)
+{
+    if (array[i]>0)
+    {
+         PositiveNumbers++;
+    }
+    
+    }
+
+    Console.WriteLine($"Количество положительных чисел равно { PositiveNumbers}");
